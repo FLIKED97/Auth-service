63f32625f61c7be4b6f3193249c8adc2726f6480
FLIKED97 <91269654+FLIKED97@users.noreply.github.com>
2025-07-09 19:58:04 +0300

    Створюю сервіс входу

---
+++ src/main/java/com/company/authservic/AuthServicApplication.java
@@ -0,0 +1,13 @@
+package com.company.authservic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/authservic/AuthServicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.authservic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
